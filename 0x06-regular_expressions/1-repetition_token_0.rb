a7489aff100b771b180c0b492a609db9f5194df4
Celeekeybor <celestinbor02@gmail.com>
2023-08-02 12:48:33 -0700

    my readme file

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env ruby
-# This script accepts one argument and passes it to a regular expression,
-# matching method
-# The regular expression must match the given cases
-
-puts ARGV[0].scan(/hbt{2,5}n/).join

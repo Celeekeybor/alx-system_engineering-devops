28d0fdc35bcf617ddc4b65993f3d931c19ae9ec5
Celeekeybor <celestinbor02@gmail.com>
2023-09-23 07:57:52 -0700

    0 created file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
-# Create a file called holberton in /tmp
-file { '/tmp/holberton':
+# using puppet create a file in /tmp
+file { '/tmp/school':
+  ensure  => 'file',
+  content => 'I love Puppet',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet',
 }

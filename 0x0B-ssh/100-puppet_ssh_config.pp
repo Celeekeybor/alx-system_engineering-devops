b9704860e395385b14dd7b11dbb3f4ce5aa59903
Celeekeybor <celestinbor02@gmail.com>
2023-09-27 11:22:14 -0700

    using puppet to make changes

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# using puppet to make some changes
+file_line {'Turn off passwd auth':
+    ensure => 'present',
+    path   => '/etc/ssh/ssh_config',
+    line   => '    PasswordAuthentication no',
+    match  =>  'PasswordAuthentication yes',
+}
+file_line {'Declare identity file':
+    ensure => 'present',
+    path   => '/etc/ssh/ssh_config',
+    line   => '    IdentityFile ~/.ssh/school',
+}
